72f29d62b16578a6c1cb6e1bf497bef13fcff347
Henry Izere <henry.izere@maine.edu>
2023-09-04 12:03:10 -0400

    changed a few variable and added comments for readability

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ class MCP3008:
             voltage in range [0, 1023] where 1023 = VREF (3V3)
         """
         self.cs.value(0)  # select and ready to communicate
-        self._out_buf[1] = ((not is_differential) << 7) | (pin << 4)#
+        self._out_buf[1] = ((not is_differential) << 7) | (pin << 4)
         self._spi.write_readinto(self._out_buf, self._in_buf)
         self.cs.value(1)  # turn off or end communication
         return ((self._in_buf[1] & 0x03) << 8) | self._in_buf[2]
@@ -44,7 +44,7 @@ class MCP3008:
 spi = SPI(0, sck=Pin(2), mosi=Pin(3), miso=Pin(4), baudrate=100000)
 cs = Pin(22, Pin.OUT)#can be any GPIO pins on the board
 cs.value(1)  # disable chip at start
-chip = MCP3008(spi, cs)
+mcp3008 = MCP3008(spi, cs)
 #end of spi connection
 
 #start of I2C configuration for the oled
@@ -54,26 +54,26 @@ i2c = I2C(0, scl = Pin(17), sda = Pin(16), freq = 200000)
 oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)
 led = Pin('LED', Pin.OUT)
 
-def Format_Voltage(actual):
-    voltage = (actual / 1023.0) * 3.3
+def Format_Voltage(value):#formats the adc value back into voltage for reference
+    voltage = (value / 1023.0) * 3.3
     voltage_str = "{:.2f}".format(voltage)
     formatted_Voltage = float(voltage_str)
     return formatted_Voltage
 
 while True:
-    led.off()
-    actual = chip.read(0)
-    formatted_Voltage = Format_Voltage(actual)
-    oled.text(f"ADC Value: {actual}",0,0)
+    led.off()#turn off the LED. it is to make sure the code is working andf for debugging purposes
+    value = mcp3008.read(0)
+    formatted_Voltage = Format_Voltage(value)
+    oled.text(f"ADC Value: {value}",0,0)
     oled.text(f"Voltage: {formatted_Voltage}", 0, 30)#middle left
     oled.show()
     utime.sleep(0.5)
     led.on()
     oled.fill(0) #clear whatever is there
     oled.show()
-    actual = chip.read(0)
-    formatted_Voltageb = Format_Voltage(actual)
-    oled.text(f"ADC Value: {actual}",0,0)#top left
+    value = mcp3008.read(0)
+    formatted_Voltageb = Format_Voltage(value)
+    oled.text(f"ADC Value: {value}",0,0)#top left
     oled.text(f"Voltage: {formatted_Voltage}", 0, 30)#middle left
     oled.show()
     utime.sleep(0.5)
