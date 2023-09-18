449ffd52572eb146f4f2d7f43674a1aaf542c032
Henry Izere <henry.izere@maine.edu>
2023-09-18 10:40:02 -0400

    Changed errors in how I pulse the Red and IR LED now they pulse at 50 HZ concurrently

---
+++ LEDConnect.py
@@ -0,0 +1,22 @@
+import machine
+import time
+
+# Set up GPIO 14 and 15 as outputs
+pin14 = machine.Pin(14, machine.Pin.OUT)
+pin15 = machine.Pin(15, machine.Pin.OUT)
+
+# Initial states: GPIO14 is high, GPIO15 is low
+pin14.high()
+pin15.low()
+
+try:
+    while True:
+        # Toggle the states of the pins
+        pin14.toggle()
+        pin15.toggle()
+        time.sleep(0.01)  # 10ms delay for 50Hz frequency
+
+except KeyboardInterrupt:
+    # Reset the pins to low before exiting
+    pin14.low()
+    pin15.low()
