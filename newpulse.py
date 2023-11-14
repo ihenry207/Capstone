d64ccf65cb7e214ce4509a5640255945c2688096
Henry Izere <henry.izere@maine.edu>
2023-11-13 20:07:10 -0500

    I added a new working of called newPulse.py this code contains the final version of the working code

--- newpulse.py
+++ newpulse.py
@@ -48,10 +48,10 @@ def LED_switch(Window_Size):
     readings_to_discard = 300#since we are reading 3 times when it's high
     valid_red_buffer = red_buffer[readings_to_discard:]
     valid_ir_buffer = ir_buffer[readings_to_discard:]
-    print("printing ir buffer numbers\n\n\n\n")
+    print("printing ir buffer numbers\n\n")
     print(valid_ir_buffer)
-    print("\n\n\n\n\n")
-    print("printing red buffer numbers\n\n\n\n")
+    print("\n\n\n")
+    print("printing red buffer numbers\n\n\n")
     print(valid_red_buffer)
     return valid_red_buffer, valid_ir_buffer
 
@@ -81,7 +81,7 @@ def find_peaks(values, threshold, min_distance):
 
 def find_heart_rate(values):
     # Redefine the threshold and minimum distance
-    threshold = 50600#according to matlab, threshold can change anytime
+    threshold = 36000#according to matlab, threshold can change anytime
     min_distance = 60  # This corresponds to 0.6 seconds given a sampling rate of 100Hz
 
     # Find the peaks using the custom function with the new parameters
@@ -145,13 +145,29 @@ def find_spo2(red_values, ir_values):
     
     return estimated_spo2
 
+def new_spo2(red_values, ir_values):
+    R1 = (max(red_values) - min(red_values)) * (min(ir_values))
+    R2 = (max(ir_values) - min(ir_values)) * (min(red_values))
+     # Handle division by zero
+    if R2 == 0:
+        print("Error: Division by zero encountered. Cannot calculate SpO2.")
+        return None
+
+    R = R1 / R2
+    sqR = R**2
+    cubeR = R**3
+    spo2 = (10*cubeR)-(53*sqR)+(43*R)+98
+    return spo2
+
 while True:
-    #red_values, ir_values = LED_switch(WINDOW_SIZE)
-    spo2 = find_spo2(red_values, ir_values)
+    red_values, ir_values = LED_switch(WINDOW_SIZE)
+    
+    spo2 = new_spo2(red_values, ir_values)
+    #spo2 = find_spo2(red_values, ir_values)
     bpm = find_heart_rate(red_values)
     print("Estimated Heart Rate: {:.2f} BPM".format(bpm))
     print("Estimated SpO2 Level: {:.2f}% ".format(spo2))
-    #find_spo2(red_values, red_peaks, ir_values, ir_peaks)
+    #find_spo2(red _values, red_peaks, ir_values, ir_peaks)
    # Display the heart rate on the OLED
     oled.fill(0)  # Clear the display
     oled.text("Heart Rate: ", 0, 0)  # Top left
